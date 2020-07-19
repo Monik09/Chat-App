4d32e9eba9ff2c20a56ec0cc0495298fc62110ed
Monik09 <monikinderjitsingh5@gmail.com>
2020-07-19 15:41:52 +0530

    Differentiating sender and receiver

--- lib/widgets/chat/messge_bubble.dart
+++ lib/widgets/chat/messge_bubble.dart
@@ -2,20 +2,36 @@ import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
   final String msg;
-  MessageBubble(this.msg);
+  final bool isMe;
+  final Key key;
+  MessageBubble({this.msg, this.isMe,this.key});
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: [
         Container(
           decoration: BoxDecoration(
-            color: Theme.of(context).accentColor,
-            borderRadius: BorderRadius.circular(12),
+            color:
+                isMe == true ? Colors.grey[300] : Theme.of(context).accentColor,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12),
+              topRight: Radius.circular(12),
+              bottomLeft: isMe ? Radius.circular(12) : Radius.circular(0),
+              bottomRight: !isMe ? Radius.circular(12) : Radius.circular(0),
+            ),
           ),
           width: 140,
           margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
           padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-          child: Text(msg,style: TextStyle(color: Theme.of(context).accentTextTheme.headline1.color,),),
+          child: Text(
+            msg,
+            style: TextStyle(
+              color: isMe == true
+                  ? Colors.black
+                  : Theme.of(context).accentTextTheme.headline1.color,
+            ),
+          ),
         ),
       ],
     );
